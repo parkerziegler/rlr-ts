13e56bdb783957b6074e9e119494db7e9d7f4eed
parkerziegler <parkerellisziegler@gmail.com>
2018-02-06 07:44:41 -0800

    Initial commit.

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import 'normalize.css/normalize.css';
+import './global.css';
+
+import { render } from 'react-dom';
+
+/* Invert comments for immutable */
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { routerForBrowser, initializeCurrentLocation, Location } from 'redux-little-router';
+// import { createStore, compose, applyMiddleware } from 'redux';
+// import { combineReducers } from 'redux-immutable';
+// import { Map, fromJS } from 'immutable';
+// import { immutableRouterForBrowser, initializeCurrentLocation } from '../../src';
+
+import routes from './routes';
+import wrap from './wrap';
+import Demo from './demo';
+
+/* Invert comments for immutable */
+const { reducer, enhancer, middleware } = routerForBrowser({ routes });
+const initialState = (window as any).__INITIAL_STATE || {};
+// const { reducer, enhancer, middleware } = immutableRouterForBrowser({ routes });
+// const initialState = window.__INITIAL_STATE ? fromJS(window.__INITIAL_STATE) : Map();
+
+const store = createStore(
+  combineReducers({ router: reducer }),
+  // If this is a server render, we grab the
+  // initial state the hbs template inserted
+  initialState,
+  compose(
+    enhancer,
+    applyMiddleware(middleware),
+    (window as any).devToolsExtension ? (window as any).devToolsExtension() : f => f
+  )
+);
+
+/* Invert comments for immutable */
+const initialLocation: Location = store.getState().router;
+// const initialLocation = store.getState().get('router').toJS();
+
+if (initialLocation) {
+  store.dispatch(initializeCurrentLocation(initialLocation));
+}
+
+render(wrap(store)(Demo), document.getElementById('content'));
---
+++ src/wrap.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Provider } from 'react-redux';
+
+export default (store: any) => (Root: any) =>
+  <Provider store={store}>
+    <Root />
+  </Provider>;
