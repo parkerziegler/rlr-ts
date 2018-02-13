cf7b73a402c564212d623f24506ddad242c3ccb1
parkerziegler <parkerellisziegler@gmail.com>
2018-02-12 19:20:53 -0800

    Revert master to regular demo.

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,21 @@ import { render } from 'react-dom';
 
 /* Invert comments for immutable */
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import { routerForBrowser, initializeCurrentLocation, Location, immutableRouterForBrowser } from 'redux-little-router';
+import { routerForBrowser, initializeCurrentLocation, Location } from 'redux-little-router';
+// import { immutableRouterForBrowser } from 'redux-little-router/es/immutable';
 // import { createStore, compose, applyMiddleware } from 'redux';
 // import { combineReducers } from 'redux-immutable';
-import { Map, fromJS } from 'immutable';
-// import { immutableRouterForBrowser, initializeCurrentLocation } from '../../src';
+// import { Map, fromJS } from 'immutable';
 
 import routes from './routes';
 import wrap from './wrap';
 import Demo from './demo';
 
 /* Invert comments for immutable */
-// const { reducer, enhancer, middleware } = routerForBrowser({ routes });
-// const initialState = (window as any).__INITIAL_STATE || {};
-const { reducer, enhancer, middleware } = immutableRouterForBrowser({ routes });
-const initialState = (window as any).__INITIAL_STATE ? fromJS((window as any).__INITIAL_STATE) : Map();
+const { reducer, enhancer, middleware } = routerForBrowser({ routes });
+const initialState = (window as any).__INITIAL_STATE || {};
+// const { reducer, enhancer, middleware } = immutableRouterForBrowser({ routes });
+// const initialState = (window as any).__INITIAL_STATE ? fromJS((window as any).__INITIAL_STATE) : Map();
 
 const store = createStore(
   combineReducers({ router: reducer }),
